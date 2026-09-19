f26e374104a83ff00019ca8b9aad86e2c89a16c3
Misker <Miskernegash1@gmail.com>
2026-09-19 15:40:57 -0400

    Add UPC input

---
+++ Lab8_mnegash5-1.py
@@ -0,0 +1,9 @@
+"""
+Program Name: UPC Validator
+Author: Misker Negash
+Purpose: This program checks whether a 12-digit UPC-A code is valid.
+Starter Code: No starter code was used.
+Date: September 19, 2026
+"""
+
+upc = input("Enter a 12-digit UPC: ")
