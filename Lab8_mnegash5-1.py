d278322ac859f0b2bb446244c439d86259137f55
Misker <Miskernegash1@gmail.com>
2026-09-19 17:18:52 -0400

    Add digits 2, 4, 6, 8, and 10

--- Lab8_mnegash5-1.py
+++ Lab8_mnegash5-1.py
@@ -2,7 +2,6 @@
 Program Name: UPC Validator
 Author: Misker Negash
 Purpose: This program checks whether a 12-digit UPC-A code is valid.
-Starter Code: No starter code was used.
 Date: September 19, 2026
 """
 
@@ -14,3 +13,17 @@ def find_upc(first_11):
         odd_total = odd_total + int(first_11[i])
 
     odd_total = odd_total * 3
+# Add digits 2, 4, 6, 8, and 10
+    for i in range(1, 11, 2):
+        even_total = even_total + int(first_11[i])
+
+    total = odd_total + even_total
+
+    remainder = total % 10
+
+    if remainder == 0:
+        check_digit = 0
+    else:
+        check_digit = 10 - remainder
+
+    return check_digit
