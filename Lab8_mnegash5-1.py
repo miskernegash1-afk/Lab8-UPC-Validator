0f352dab702241a004ecc6be54283e1e6cddada7
Misker <Miskernegash1@gmail.com>
2026-09-19 16:12:16 -0400

    Add digits 1, 3, 5, 7, 9, and 11

--- Lab8_mnegash5-1.py
+++ Lab8_mnegash5-1.py
@@ -7,3 +7,10 @@ Date: September 19, 2026
 """
 
 upc = input("Enter a 12-digit UPC: ")
+def find_upc(first_11):
+    odd_total = 0
+    even_total = 0
+    for i in range(0, 11, 2):
+        odd_total = odd_total + int(first_11[i])
+
+    odd_total = odd_total * 3
